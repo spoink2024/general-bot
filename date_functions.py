ce98e199958f452d9e9bff404f0521da7b3d141e
spoink2022 <57078041+spoink2022@users.noreply.github.com>
2019-10-27 21:46:33 -0400

    Add files via upload

---
+++ date_functions.py
@@ -0,0 +1,7 @@
+import random
+def list_primes(start, end):
+    for i in range(start, end+1):
+        print(i)
+        
+def random_square():
+    print(random.randint(1, 100) ** 2)
---
+++ math_functions.py
@@ -0,0 +1,15 @@
+import random
+def math_list_primes(start, end):
+    tmp = 1
+    if start > end:
+        tmp = -1
+    for i in range(start, end+1, tmp):
+        prime = True
+        for j in range(2, i):
+            if(i%j==0):
+                prime = False
+        if (prime or i==2 or i==3) and not i==1:
+            print(i)
+
+def math_random_square():
+    print(random.randint(1, 100) ** 2)
